01b46259a1a985d606756cda649cca7258cab80a
Daniel Wesson <danielwesson@MacBook-Air-Daniel.local>
2021-03-12 21:49:40 +0300

    Create welcome section and slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,37 @@
 
 import $ from 'jquery'
+import AOS from 'aos'
+import Slick from 'slick-carousel'
 
 $(document).ready(() => {
-	
+	AOS.init({
+		once: true,
+		duration: 800
+	})
+
+	$('#section_projects .slider .slides').slick({
+		infinite: true,
+		autoplay: false,
+		autoplaySpeed: 5000,
+		// pauseOnFocus: false,
+		// pauseOnHover: true,
+		// pauseOnDotsHover: true,
+		fade: true,
+		// swipe: true,
+		// touchMove: true,
+		useTransform: false,
+		speed: 700,
+		// appendArrows: $('#section-welcome .slider-arrows'),
+		prevArrow: $('#section_projects .slider .navigation .prev'),
+		nextArrow: $('#section_projects .slider .navigation .next'),
+		dots: true,
+		appendDots: $('#section_projects .slider .dots'),
+		// 
+		// responsive: [{
+		// 	breakpoint: 576,
+		// 	settings: {
+		// 		swipe: true,
+		// 	}
+		// }]
+	});
 })
